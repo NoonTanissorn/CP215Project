809969dc4c14ac63ddcea9d7d978e2584fd45419
Noon Tanissorn <51069897+NoonTanissorn@users.noreply.github.com>
2024-10-30 20:29:02 +0700

    Initial Project

---
+++ Game1.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using ThanaNita.MonoGameTnt;
+
+namespace CP215Project
+{
+    public class Game1 : Game2D
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new CP215Project.Game1();
+game.Run();
