ffe02c82de3439820acfe23889437ff16ce597d9
Noon Tanissorn <51069897+NoonTanissorn@users.noreply.github.com>
2024-11-24 22:43:43 +0700

    Noon1: เพิ่ม Monstor

--- Game1.cs
+++ Game1.cs
@@ -7,5 +7,48 @@ namespace CP215Project
 {
     public class Game1 : Game2D
     {
+            protected override void LoadContent()
+            {
+                //TestAladdin();
+                //TestGirl();
+                //All.Add(new Aladdin(ScreenSize / 2));
+                All.Add(new Monstor(ScreenSize / 2));
+                //All.Add(new Exercise(ScreenSize / 4));
+                //All.Add(new Fireball(new Vector2(1000, 180)));
+            }
+
+            private void TestAladdin()
+            {
+                var size = new Vector2(45, 45);
+                var sprite = new SpriteActor();
+                sprite.Position = ScreenSize / 2;
+                sprite.Origin = size / 2;
+                sprite.Scale = new Vector2(4, 4);
+                All.Add(sprite);
+
+                var texture = TextureCache.Get("Aladdin.png");
+                var regions2d = RegionCutter.Cut(texture, size, countX: 4, countY: 4);
+                var regions1d = RegionSelector.Select(regions2d, start: 5, count: 8);
+                var animation = new Animation(sprite, 0.5f, regions1d);
+                sprite.AddAction(animation);
+            }
+
+            private void TestGirl()
+            {
+                var size = new Vector2(60, 60);
+                var sprite = new SpriteActor();
+                sprite.Position = ScreenSize / 2;
+                sprite.Origin = size / 2;
+                sprite.Scale = new Vector2(2, 2);
+                All.Add(sprite);
+
+                var texture = TextureCache.Get("Girl.png");
+                var regions2d = RegionCutter.Cut(texture, size, countX: 8, countY: 4);
+                var regions1d = RegionSelector.Select(regions2d, start: 16, count: 8);
+                var animation = new Animation(sprite, 0.5f, regions1d);
+                sprite.AddAction(animation);
+            }
+
+        }
     }
-}
+
---
+++ Monstor.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThanaNita.MonoGameTnt;
+
+namespace CP215Project
+{
+    public class Monstor : SpriteActor
+    {
+        //Animation[] animationArray;
+        AnimationStates states;
+        public Monstor(Vector2 position)
+        {
+            var size = new Vector2(100, 150);
+            var sprite = this;
+            sprite.Position = position;
+            sprite.Origin = size / 2;
+            sprite.Scale = new Vector2(1, 1f);
+
+            var texture = TextureCache.Get("Monstor.png");
+            var regions2d = RegionCutter.Cut(texture, size, countX: 4, countY: 4);
+
+            var selector = new RegionSelector(regions2d);
+            var stay = new Animation(sprite, 0.5f, selector.Select1by1(0, 2));
+            var left = new Animation(sprite, 0.5f, selector.Select(start: 4, count: 4));
+            var right = new Animation(sprite, 0.5f, selector.Select(start: 8, count: 4));
+            var down = new Animation(sprite, 0.5f, selector.Select(start: 12, count: 4));
+            var up = new Animation(sprite, 0.5f, selector.Select(start: 0, count: 4));
+            states = new AnimationStates([stay, left, right, down, up]);
+            AddAction(states);
+
+            sprite.AddAction(stay);
+            sprite.AddAction(left);
+            sprite.AddAction(right);
+            right.Running = false;
+            left.Running = false;
+        }
+        /*int last = -1;
+        public void Animate(int index)
+        {
+            for (int i = 0; i < animationArray.Length; i++)
+            {
+                animationArray[i].Running = i == index;
+            }
+
+            if (index != last)
+            {
+                animationArray[index].Restart();
+            }
+            last = index;
+        }*/
+
+        public override void Act(float deltaTime)
+        {
+            base.Act(deltaTime);
+
+            var direction = DirectionKey.Normalized;
+            Position += direction * 1000 * deltaTime;
+            if (direction.X > 0)
+            {
+                states.Animate(2);
+            }
+            else if (direction.X < 0)
+            {
+                states.Animate(1);
+            }
+            else if (direction.Y > 0)
+            {
+                states.Animate(4);
+            }
+            else if (direction.Y < 0)
+            {
+                states.Animate(3);
+            }
+            else
+            {
+                states.Animate(0);
+            }
+        }
+
+    }
+}
