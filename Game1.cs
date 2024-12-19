5b130ec4f668706984c92bc23f8abfb39d062ac8
Noon Tanissorn <51069897+NoonTanissorn@users.noreply.github.com>
2024-12-20 00:41:06 +0700

    Update2

--- Game1.cs
+++ Game1.cs
@@ -7,7 +7,8 @@ namespace CP215Project
 {
     public class Game1 : Game2D
     {
-        Actor room1, room2, room4, room6, room12, gameover;
+        Actor room1, room2, room3, room4, room5, room6, room12, gameover;
+        CameraMan cameraMan;
         protected override void LoadContent()
         {
             cameraMan = new CameraMan(Camera, ScreenSize);
@@ -16,26 +17,24 @@ namespace CP215Project
             room12 = new Room12();
             All.Add(room12);
             */
-            
-            room1 = new Room1(ExitNotifier, cameraMan);
+
+
+            /*
+            room1 = new Room1(ScreenSize, ExitNotifier, cameraMan);
             All.Add(room1);
             */
+
             /*
-             room2 = new Room2(ExitNotifier);
+             room2 = new Room2(ScreenSize, ExitNotifier, cameraMan);
             All.Add(room2);
             */
             /*
-            room4 = new Room4();
+            room4 = new Room4(ScreenSize, ExitNotifier, cameraMan);
             All.Add(room4);
             */
-            
-            room6 = new Room6();
+
+            room6 = new Room6(ScreenSize, ExitNotifier, cameraMan);
             All.Add(room6);
-            
-
-            
-            
-
         }
 
         
@@ -88,7 +87,7 @@ namespace CP215Project
             {
                 gameover.Detach();
                 gameover = null;
-                room1 = new Room1(ExitNotifier, cameraMan);
+                room1 = new Room1(ScreenSize, ExitNotifier, cameraMan);
                 All.Add(room1);
             }
 
