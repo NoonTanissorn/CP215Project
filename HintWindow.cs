83c24ca1227528fad7ea57600f0233fbb662b656
cocoayaa <areeya.tre@gmail.com>
2024-12-20 16:47:09 +0700

    คำถามห้อง 1

--- HintWindow.cs
+++ HintWindow.cs
@@ -10,31 +10,34 @@ namespace CP215Project
 
     {
         public HintWindow()
-            : base(new Vector2(300, 300), Color.Ivory, Color.Black, 2)
+            : base(new Vector2(600, 600), Color.PeachPuff , Color.Brown, 2)
         {
             CreateLabels();
         }
 
         private void CreateLabels()
         {
-            var Line1 = CreateLabel("หมาหันมา หาหมู เห็นหูหมา");
+            var Line1 = CreateLabel("[หมาหันมา หาหมู เห็นหูหมา");
             var Line2 = CreateLabel("หูหมาหนา หมูหนี หมีเห็นหมู");
             var Line3 = CreateLabel("เห็นหมูหัน หูหนี หมีหันดู");
             var Line4 = CreateLabel("หมีหมาหา เห็นหมู หันหูดี");
             var Line5 = CreateLabel("หาหมีเห็น เหม็นหู หมีหูเหม็น");
             var Line6 = CreateLabel("หาหมีหาย หมาเห็น เหม็นหูหมี");
             var Line7 = CreateLabel("หมาหนีหาย หมายหมู หูหันรี");
-            var Line8 = CreateLabel("หมายหาหมู หมาหนี หมีหายตัว");
+            var Line8 = CreateLabel("หมายหาหมู หมาหนี หมีหายตัว]");
+            var Line9 = CreateLabel("หาขาหมา หาขาหมู หาขาหมี หาขาแมว");
 
 
             Line1.Position = new Vector2(50, 50);
-            Alignment.SetPosition(Line1, AlignDirection.Down);
-            Alignment.SetPosition(Line2, AlignDirection.Down);
-            Alignment.SetPosition(Line3, AlignDirection.Down);
-            Alignment.SetPosition(Line4, AlignDirection.Down);
-            Alignment.SetPosition(Line5, AlignDirection.Down);
-            Alignment.SetPosition(Line6, AlignDirection.Down);
-            Alignment.SetPosition(Line7, AlignDirection.Down);
+            Alignment.SetPosition(Line1,Line2, AlignDirection.Down);
+
+            Alignment.SetPosition(Line2, Line3, AlignDirection.Down);
+            Alignment.SetPosition(Line3, Line4, AlignDirection.Down);
+            Alignment.SetPosition(Line4, Line5, AlignDirection.Down);
+            Alignment.SetPosition(Line5, Line6, AlignDirection.Down);
+            Alignment.SetPosition(Line6, Line7, AlignDirection.Down);
+            Alignment.SetPosition(Line7, Line8, AlignDirection.Down);
+            Alignment.SetPosition(Line8, Line9, AlignDirection.Down);
 
             Add(Line1);
             Add(Line2);
@@ -44,11 +47,12 @@ namespace CP215Project
             Add(Line6);
             Add(Line7);
             Add(Line8);
+            Add(Line9);
         }
 
         private Text CreateLabel(string s)
         {
-            return new Text("Pridi-Regular.ttf", 25, Color.Black, s);
+            return new Text("Pridi-Regular.ttf", 50, Color.Black, s);
         }
 
 
